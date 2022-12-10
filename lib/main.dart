cffa047d046518cdda67340ce39a708f486478b7
Phạm Minh Đạt <phamminhdat@MacBook-Pro-cua-Pham.local>
2022-12-10 21:47:39 +0700

    upload notification

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,92 @@
-import 'package:flutter/material.dart';
+import 'dart:io' show Platform;
+import 'dart:math';
 
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:firebase_messaging1/screen_two.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+
+  debugPrint("Handling a background message: ${message.messageId}");
+}
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  //Init Firebase messaging.
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+  NotificationSettings settings = await messaging.requestPermission();
+
+  //Init Foreground messaging IOS.
+  await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+    alert: true,
+    badge: true,
+    sound: true,
+  );
+
+  //Init Local notification (Foreground messaging Android).
+  const settingsAndroid = AndroidInitializationSettings('@mipmap/ic_launcher');
+  const initSettings = InitializationSettings(android: settingsAndroid);
+  final flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+  if (Platform.isAndroid) {
+    await flutterLocalNotificationsPlugin.initialize(
+      initSettings,
+      onDidReceiveNotificationResponse: (val){
+        debugPrint('hello ${val.payload}');
+      }
+    );
+  }
+  Random random = Random();
+
+  //Handle notification.
+  if (settings.authorizationStatus == AuthorizationStatus.authorized) {
+    //Foreground State. - App running.
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) async {
+      debugPrint('Got a message whilst in the foreground!');
+
+      if (message.notification != null) {
+        if (Platform.isAndroid) {
+          const androidNotificationDetails = AndroidNotificationDetails(
+            'your channel id',
+            'your channel name',
+            channelDescription: 'your channel description',
+            importance: Importance.max,
+            priority: Priority.high,
+            ticker: 'ticker',
+          );
+          const notificationDetails =
+              NotificationDetails(android: androidNotificationDetails);
+          await flutterLocalNotificationsPlugin.show(
+            random.nextInt(10000),
+            message.notification!.title!,
+            message.notification!.body!,
+            notificationDetails,
+            payload: 'datpm'
+          );
+        }
+      }
+    });
+
+    //Background State and Terminated State.
+    FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
+    //Click notification Background State.
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) async {
+      if (message.notification != null) {
+        debugPrint(
+            "open app when click notification: ${message.notification!.title}");
+      }
+    });
+  } else if (settings.authorizationStatus == AuthorizationStatus.provisional) {
+    debugPrint('User granted provisional permission');
+  } else {
+    debugPrint('User declined or has not accepted permission');
+  }
+
   runApp(const MyApp());
 }
 
@@ -25,20 +111,34 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  Future<void> setupInteractedMessage() async {
+    //Handle terminated state.
+    final fcmMess = await FirebaseMessaging.instance.getInitialMessage();
+    if (fcmMess != null) _handleMessage(fcmMess);
+
+    //Handle background state.
+    FirebaseMessaging.onMessageOpenedApp.listen(_handleMessage);
+  }
+
+  void _handleMessage(RemoteMessage message) {
+    Navigator.push(
+        context, MaterialPageRoute(builder: (context) => ScreenTwo()));
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setupInteractedMessage();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Firebase messaging'),
       ),
-      body: Center(
-        child: Column(
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-          ],
-        ),
+      body: const Center(
+        child: Text('Notification: 111'),
       ),
     );
   }
